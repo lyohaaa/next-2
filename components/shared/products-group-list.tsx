50424bec54c12faf7aa951d15120704b58445be6
git07tany <anashkina_2007@mail.ru>
2025-04-03 12:25:30 +0300

    tenth commit

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Title } from './title';
 import { ProductCard } from './product-card';
+import { useIntersection } from 'react-use';
 import { cn } from '@/lib/utils';
 
 interface Props {
@@ -19,7 +20,18 @@ export const ProductsGroupList: React.FC<Props> = ({
     categoryId,
     className
  }) => {
-    return <div className={className}>
+    const intersectionRef = React.useRef(null);
+    const intersection = useIntersection(intersectionRef, {
+        threshold: 0.4,
+    });
+
+    React.useEffect(() => {
+        if (intersection?.isIntersecting) {
+            console.log(title, categoryId);
+        }
+    }, [categoryId, intersection?.isIntersecting, title]);
+
+    return <div className={className} id={title}>
         <Title text={title} size="lg" className="font-extrabold mb-5" />
 
         <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
