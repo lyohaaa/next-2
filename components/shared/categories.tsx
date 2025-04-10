d56c659331cbc52e4e06d7367ac9c24e04579eb5
git07tany <anashkina_2007@mail.ru>
2025-04-10 12:26:39 +0300

    11th commit

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -22,16 +22,17 @@ export const Categories: React.FC<Props> = ({className}) => {
     const categoryActiveId = useCategoryStore((state) => state.activeId);
     return (
         <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
-            {cats.map((cat, index) => (
+            {cats.map(({name, id}, index) => (
                 <a
                 key={index}
                 className={cn(
                     'flex items-center font-bold h-11 rounded-2xl px-5',
                     categoryActiveId === index + 1 && 'bg-white shadow-md shadow-gray-200 text-primary',
                 )}
+                href = {`#${name}`}
                 >
                     <button>
-                        {cat}
+                        {name}
                     </button>
                 </a>
             ))}
